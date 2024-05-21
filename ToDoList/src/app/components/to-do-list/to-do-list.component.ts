458aeb2dbe03122e188a20bca85a044030eb4924
tayushevamv <tayushevamv@yandex.ru>
2024-05-21 19:54:41 +0400

    промежуточный фикс

--- ToDoList/src/app/components/to-do-list/to-do-list.component.ts
+++ ToDoList/src/app/components/to-do-list/to-do-list.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { AppComponent } from 'src/app/app.component';
 
+type listElement = {id: number, text: string};
+let elements: Array<listElement>;
+
 @Component({
   selector: 'app-to-do-list',
   templateUrl: './to-do-list.component.html',
@@ -8,4 +11,9 @@ import { AppComponent } from 'src/app/app.component';
 })
 export class ToDoListComponent {
 
+ elements = [ {id: 1, text: 'By a new gaming laptop'}, 
+              {id: 2, text: 'Complete previous task'}, 
+              {id: 3, text: 'Create some angular app'}
+            ];
 }
+
