68f8be8942227ccd0777a87277f0e13ba9a38d6f
tayushevamv <tayushevamv@yandex.ru>
2024-05-28 08:45:44 +0400

    промежуточный

--- ToDoList/src/app/app.module.ts
+++ ToDoList/src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ToDoListComponent } from './components/to-do-list/to-do-list.component';
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 @NgModule({
   declarations: [
@@ -10,9 +15,15 @@ import { ToDoListComponent } from './components/to-do-list/to-do-list.component'
     ToDoListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatFormFieldModule
   ],
-  providers: [],
+  providers: [
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {appearance: 'outline'}}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- ToDoList/src/app/components/to-do-list/to-do-list.component.ts
+++ ToDoList/src/app/components/to-do-list/to-do-list.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-import { AppComponent } from 'src/app/app.component';
 
 type listElement = {id: number, text: string};
-let elements: Array<listElement>;
 
 @Component({
   selector: 'app-to-do-list',
@@ -11,9 +9,26 @@ let elements: Array<listElement>;
 })
 export class ToDoListComponent {
 
- elements = [ {id: 1, text: 'By a new gaming laptop'}, 
-              {id: 2, text: 'Complete previous task'}, 
-              {id: 3, text: 'Create some angular app'}
-            ];
+  newValue: string = '';
+
+  elements: Array<listElement> = [ {id: 1, text: 'By a new gaming laptop'}, 
+                              {id: 2, text: 'Complete previous task'}, 
+                              {id: 3, text: 'Create some angular app'}
+                            ];
+
+  addItem() : void {
+    if (this.newValue) {
+      let newId: number = Math.max(...this.elements.map((element)=> element.id))+1;
+      this.elements.push(
+              {id: newId, text: this.newValue}
+          );
+    }
+  }
+
+  deleteItem(id: number) : void {
+    console.log('delItem ' + id);
+   // elements.[id]
+  }
+
 }
 
